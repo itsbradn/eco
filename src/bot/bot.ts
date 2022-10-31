6c2128dc80cbf00f87a07fc8f240e43b6042a405
braden <98288965+itsbradn@users.noreply.github.com>
2022-10-30 18:22:53 -0700

    [🛒] Added colors & interactions collection to bot

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -4,7 +4,7 @@ import { createLogger } from "discordeno/logger";
 import { setupAnalyticsHooks } from "../analytics";
 import { INTENTS, REST_URL } from "../configs";
 import { setupEventHandlers } from "./events/mod";
-import { MessageCollector } from "./utils/collectors";
+import { InteractionCollector, MessageCollector } from "./utils/collectors";
 import { customizeInternals } from "./utils/internals/mod";
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN as string;
@@ -24,8 +24,16 @@ export const bot = enableHelpersPlugin(
 function customizeBot<B extends Bot = Bot>(bot: B): BotWithCustomProps {
   const customized = bot as unknown as BotWithCustomProps;
   customized.logger = createLogger({ name: "[Bot]" });
+  customized.colors = {
+    default: 0x4DCCBD,
+				success: 0x60d394,
+				log: 0x9381ff,
+				warn: 0xffd670,
+				error: 0xee6055,
+  }
   customized.collectors = {
     messages: new Collection(),
+    interactions: new Collection(),
   };
   customized.commandVersions = new Collection();
 
@@ -41,7 +49,16 @@ export type BotWithCustomProps<B extends Bot = Bot> = B & {
   collectors: {
     /** Holds the pending messages collectors that users can respond to. */
     messages: Collection<bigint, MessageCollector>;
+    interactions: Collection<bigint, InteractionCollector>;
   };
+
+  colors: {
+    default: number,
+				success: number,
+				log: number,
+				warn: number,
+				error: number,
+  }
   /** The command versions for each guild id. */
   commandVersions: Collection<bigint, number>;
 };
