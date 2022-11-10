4c50d253932581de25186924f0f7780bc815ea33
braden <98288965+itsbradn@users.noreply.github.com>
2022-11-10 00:57:41 -0800

    [🎁] Updated present chance on perfect work

--- src/bot/commands/work.ts
+++ src/bot/commands/work.ts
@@ -134,7 +134,7 @@ export default createCommand({
 			};
 			if (result.didLevelUp) rewards.value += `⭐ Leveled Up!\n`;
 			if (result.gotRaise) rewards.value += `💼 You got a raise!\n`;
-			if (percentChance(10)) {
+			if (percentChance(70)) {
 				user.inventory.add('CheapPresent', 1);
 				rewards.value += `🎁 +1 Cheap Present\n`;
 			}
