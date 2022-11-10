13f9f06a57b57f1c696dcd8a9f6696bd9f7273c3
braden <98288965+itsbradn@users.noreply.github.com>
2022-11-10 00:55:48 -0800

    [👜] Added Inventory checking & deletion of items

--- src/bot/managers/User/InventoryManager.ts
+++ src/bot/managers/User/InventoryManager.ts
@@ -8,6 +8,10 @@ export class UserInventoryManager extends UserDataManager {
 		return itemFound.amount;
 	}
 
+	get inventory() {
+		return this.model.storage.items;
+	}
+
 	add (name: itemType, amount: number) {
 		const itemFound = this.model.storage.items.find((v) => v.name === name);
 		if (!itemFound) {
@@ -29,5 +33,28 @@ export class UserInventoryManager extends UserDataManager {
 		this.model.storage.items[this.model.storage.items.indexOf(itemFound) ?? 0] = added;
 		this.model.markModified('storage.items');
 		return added.amount;
+	}
+
+	del (name: itemType, amount: number) {
+		const itemFound = this.model.storage.items.find((v) => v.name === name);
+		if (!itemFound) {
+			this.model.storage.items.push({
+				name,
+				amount: 0,
+			});
+
+			this.model.markModified('storage.items');
+
+			return 0;
+		}
+
+		const removed = {
+			name,
+			amount: itemFound.amount - amount,
+		}
+
+		this.model.storage.items[this.model.storage.items.indexOf(itemFound) ?? 0] = removed;
+		this.model.markModified('storage.items');
+		return removed.amount;
 	}
 }
