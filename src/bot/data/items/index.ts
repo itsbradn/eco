690d962c0ce30819bdf68a65e837bd334b0391f6
braden <98288965+itsbradn@users.noreply.github.com>
2022-11-12 13:24:19 -0800

    [✨] Added item array

--- src/bot/data/items/index.ts
+++ src/bot/data/items/index.ts
@@ -1,3 +1,4 @@
+import { Item } from "../../structures/item";
 import { BoxItems } from "./Boxes";
 import { MaterialItems } from "./Materials";
 import { MiningItems } from "./Mining";
@@ -9,4 +10,15 @@ export const items = {
 }
 
 export type itemType = keyof typeof items;
+
+
+const itemKeys = Object.keys(items);
+const itemValues = Object.values(items);
+export const itemArray: { key: itemType; value: Item }[] = [];
+for (let i = 0; i < itemKeys.length; i++) {
+	const itemValue = itemValues[i];
+	const itemKey = itemKeys[i];
+	if (!itemValue || !itemKey) continue;
+	itemArray.push({ key: itemKey as itemType, value: itemValue });
+}
 
