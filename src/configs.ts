001e91f420288993ef8dded7cb523564cb3bd0c8
braden <98288965+itsbradn@users.noreply.github.com>
2022-11-23 15:52:46 -0800

    [🧑‍💻] Fixed development type in configs

--- src/configs.ts
+++ src/configs.ts
@@ -8,13 +8,13 @@ export const EVENT_HANDLER_URL = `http://${process.env.EVENT_HANDLER_HOST}:${pro
 export const REST_URL = `http://${process.env.REST_HOST}:${process.env.REST_PORT}`;
 export const GATEWAY_URL = `http://${process.env.GATEWAY_HOST}:${process.env.GATEWAY_PORT}`;
 
-export const DEVELOPMENT = process.env.DEVELOPMENT ?? true;
+export const DEVELOPMENT = process.env.DEVELOPMENT === 'true';
 
 export const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${
 	process.env.MONGO_URL
 }/${DEVELOPMENT ? process.env.DEV_MONGO_DB : process.env.PROD_MONGO_DB}`;
 
-export const USER_SCHEMA_VER = process.env.USER_SCHEMA_VER ?? 1
+export const USER_SCHEMA_VER = process.env.USER_SCHEMA_VER ?? 1;
 // Gateway Proxy Configurations
 /** The gateway intents you would like to use. */
 export const INTENTS: Intents =
